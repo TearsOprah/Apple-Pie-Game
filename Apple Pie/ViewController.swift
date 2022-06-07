39e51f42b52ca2426c78809c9eccdfebb8583cb4
TearsOprah <qmeshokp@mail.ru>
2022-06-07 15:34:14 +0300

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by meshok on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
