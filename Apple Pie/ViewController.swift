18d71343009aade266a907ef747cf1d14642aca1
TearsOprah <qmeshokp@mail.ru>
2022-06-07 19:43:49 +0300

    newRound, updateUI functions

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var listOfWords = ["swift", "bug", "program", "design", "python"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    
+    var totalLosses = 0
+    
     @IBOutlet var treeImageView: UIImageView!
     
     @IBOutlet var correctWordLabel: UILabel!
@@ -20,11 +28,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound()
     }
 
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
     }
     
+    var currentGame: Game!
+    
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining:incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
 }
 
---
+++ Apple Pie/game.swift
@@ -0,0 +1,13 @@
+//
+//  game.swift
+//  Apple Pie
+//
+//  Created by meshok on 07.06.2022.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
